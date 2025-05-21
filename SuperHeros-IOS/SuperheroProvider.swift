dde91cbe097102123d8351e9788ab6756c377504
abasurco0@gmail.com <abasurco0@gmail.com>
2025-05-21 13:03:03 +0200

    prueba

---
+++ SuperHeros-IOS/SuperheroProvider.swift
@@ -0,0 +1,23 @@
+//
+//  SuperheroProvider.swift
+//  SuperHeros-IOS
+//
+//  Created by Mañanas on 21/5/25.
+//
+
+import Foundation
+
+class SuperheroProvider {
+    
+    
+    static let  API_KEY = "7606f1c6a2b57ea22ca4bdf432bd1f9c"
+
+    static func findSuperheroesByName(query: String){
+  //      let url = URL(String: "https:superheroapi.com/api/\(API_KEY)/search/")
+        
+ 
+      }
+  }
+
+
+ 
